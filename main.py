2fc3e5f5739d78d7cca51d191c8b4a4da4061efb
Sagar Patel <keyuri282017@gmail.com>
2020-12-05 16:22:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import eel
 import chempy  #module we can use for our project
+from math import *
 
 eel.init('web')
+
+def function_name1():
+  pass
+
+def function_name2():
+  pass
+
+def function_name3():
+  pass
+
+def function_name4():
+  pass
+
+def function_name5():
+  pass
+
+
 eel.start('index.html')
 
 #remember to put index.html and main.css inside of a web folder before using
