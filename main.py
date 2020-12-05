0b244500ba6de9aa786f38a59361e68de1706968
Avi Avinav <54372449+AviAvinav@users.noreply.github.com>
2020-12-05 21:58:23 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import eel
+import chempy  #module we can use for our project
+
+eel.init('web')
+eel.start('index.html')
+
+#remember to put index.html and main.css inside of a web folder before using
